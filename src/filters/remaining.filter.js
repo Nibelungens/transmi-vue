286f666c2b1aebaead2f30f4586f2e251ba71a60
Mickaël GUIHARD <guihard.m@gmail.com>
2020-09-20 15:44:52 +0200

    fix: ajout de constantes dans le filtre remaining

--- src/filters/remaining.filter.js
+++ src/filters/remaining.filter.js
@@ -1,14 +1,18 @@
+const SECOND_IN_MINUTES = 60
+const MINUTES_IN_HOUR = 60
+const HOUR_IN_DAY = 24
+
 const filter = function (value) {
     if (value < 0) return 'remaining time unknown';
     if (value === 0) return 'finish';
 
-    const minutes = Math.floor(value / 60);
-    const hours   = Math.floor(value / (60*60));
-    const days   = Math.floor(value / (60*60*24));
+    const minutes = Math.floor(value / SECOND_IN_MINUTES);
+    const hours   = Math.floor(value / (SECOND_IN_MINUTES*MINUTES_IN_HOUR));
+    const days   = Math.floor(value / (SECOND_IN_MINUTES*MINUTES_IN_HOUR*HOUR_IN_DAY));
 
-    if (days !== 0 && days !== -1) return days + ' days and ' + (hours - (24*days)) + ' hours remaining';
-    if (hours !== 0 && hours !== -1) return hours + ' hours and ' + (minutes - (60*hours)) + ' minutes remaining';
-    if (minutes !== 0 && minutes !== -1) return minutes + ' minutes and ' + (value - (60*minutes)) + ' seconds remaining';
+    if (days !== 0 && days !== -1) return days + ' days and ' + (hours - (HOUR_IN_DAY*days)) + ' hours remaining';
+    if (hours !== 0 && hours !== -1) return hours + ' hours and ' + (minutes - (MINUTES_IN_HOUR*hours)) + ' minutes remaining';
+    if (minutes !== 0 && minutes !== -1) return minutes + ' minutes and ' + (value - (SECOND_IN_MINUTES*minutes)) + ' seconds remaining';
 
     return value + ' seconds remaining';
 }
