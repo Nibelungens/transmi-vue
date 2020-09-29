2094b7929d24d01e124416938f9f59e11674c090
Mickaël GUIHARD <guihard.m@gmail.com>
2020-09-29 11:46:19 +0200

    feat: ajout d'un menu deroulant dans le pied de page

--- src/components/ui/FooterTransmission.vue
+++ src/components/ui/FooterTransmission.vue
@@ -3,9 +3,22 @@
     <b-navbar type="dark" variant="dark">
       <b-button-toolbar>
         <b-button-group size="sm" class="mr-1" >
-          <b-button :title="$t('message.footer.setting')" variant="outline-light" disabled>
-            <b-icon icon="gear-fill"></b-icon>
-          </b-button>
+          <b-dropdown size="sm" dropup :title="$t('message.footer.setting')" variant="outline-light">
+            <template v-slot:button-content>
+              <b-icon icon="gear-fill"></b-icon>
+            </template>
+            <b-dropdown-item href="#" class="m-row" disabled>{{$t('message.footer.about')}}</b-dropdown-item>
+            <b-dropdown-divider class="m-row"></b-dropdown-divider>
+            <b-dropdown-item href="#" class="m-row" disabled>{{$t('message.footer.homepage')}}</b-dropdown-item>
+            <b-dropdown-item href="#" class="m-row" disabled>{{$t('message.footer.jar')}}</b-dropdown-item>
+            <b-dropdown-divider class="m-row"></b-dropdown-divider>
+            <b-dropdown-item href="#" class="m-row" disabled>{{$t('message.footer.statistics')}}</b-dropdown-item>
+            <b-dropdown-divider class="m-row"></b-dropdown-divider>
+            <b-dropdown-item href="#" class="m-row" disabled>{{$t('message.footer.downloadRate')}}</b-dropdown-item>
+            <b-dropdown-item href="#" class="m-row" disabled>{{$t('message.footer.uploadRate')}}</b-dropdown-item>
+            <b-dropdown-divider class="m-row"></b-dropdown-divider>
+            <b-dropdown-item href="#" class="m-row" disabled>{{$t('message.footer.sortTransferts')}}</b-dropdown-item>
+          </b-dropdown>
           <b-button :title="$t('message.footer.prefs')" variant="outline-light" disabled>
             <b-icon icon="tools"></b-icon>
           </b-button>
@@ -33,5 +46,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.m-row {
+  margin: -5px -5px -5px -5px;
+  font-size: smaller;
+  border-bottom-style: hidden;
+}
 </style>
