aedf4ca8a9c3b64de113b31babf6b947b8ffa6ee
Mickaël GUIHARD <guihard.m@gmail.com>
2020-10-06 19:26:15 +0200

    feat: refactor api/jsdocs + service pour recup trackers

--- src/services/api_parent.transmission.service.js
+++ src/services/api_parent.transmission.service.js
@@ -1,5 +1,102 @@
 import * as axios from "axios";
 
+/**
+ * @typedef  {Object} Torrent
+ * @property {number} metadataPercentComplete
+ * @property {array<FileStats>} status
+ * @property {TrackerStats} trackerStats
+ * @property {number} desiredAvailable
+ * @property {number} downloadedEver
+ * @property {number} haveUnchecked
+ * @property {number} queuePosition
+ * @property {number} activityDate
+ * @property {number} dateCreated
+ * @property {number} corruptEver
+ * @property {number} uploadRatio
+ * @property {number} haveValid
+ * @property {number} startDate
+ * @property {number} totalSize
+ * @property {number} fileStats
+ * @property {Peers} peers
+ * @property {string} name
+ * @property {number} id
+ *
+ * @typedef  {Object} Peers
+ * @property {boolean} clientIsInterested
+ * @property {boolean} isDownloadingFrom
+ * @property {boolean} peerIsInterested
+ * @property {boolean} clientIsChoked
+ * @property {boolean} isUploadingTo
+ * @property {boolean} peerIsChoked
+ * @property {boolean} isEncrypted
+ * @property {number} rateToClient
+ * @property {boolean} isIncoming
+ * @property {number} rateToPeer
+ * @property {string} clientName
+ * @property {number} progress
+ * @property {string} address
+ * @property {string} flagStr
+ * @property {boolean} isUTP
+ * @property {number} port
+ *
+ * @typedef  {Object} FileStats
+ * @property {number} bytesCompleted
+ * @property {number} priority
+ * @property {boolean} wanted
+ *
+ * @typedef  {Object} TrackerStats
+ * @property {boolean} lastAnnounceSucceeded
+ * @property {number} lastAnnouncePeerCount
+ * @property {number} lastAnnounceStartTime
+ * @property {boolean} lastAnnounceTimedOut
+ * @property {boolean} lastScrapeSucceeded
+ * @property {boolean} lastScrapeTimedOut
+ * @property {number} lastScrapeStartTime
+ * @property {string} lastAnnounceResult
+ * @property {number} nextAnnounceTime
+ * @property {string} lastScrapeResult
+ * @property {number} lastAnnounceTime
+ * @property {number} lastScrapeTime
+ * @property {number} nextScrapeTime
+ * @property {boolean} hasAnnounced
+ * @property {number} downloadCount
+ * @property {number} announceState
+ * @property {number} leecherCount
+ * @property {number} scrapeState
+ * @property {number} seederCount
+ * @property {boolean} hasScraped
+ * @property {boolean} isBackup
+ * @property {string} announce
+ * @property {string} scrape
+ * @property {string} host
+ * @property {number} tier
+ * @property {number} id
+ *
+ * @typedef  {Object} Units
+ * @property {number} memory-bytes
+ * @property {array<string>} memory-units
+ * @property {number} size-bytes
+ * @property {array<string>} size-units
+ * @property {number} speed-bytes
+ * @property {array<string>} speed-units
+ *
+ * @typedef  {Object} Units
+ * @property {number} memory-bytes
+ * @property {array<string>} memory-units
+ * @property {number} size-bytes
+ * @property {array<string>} size-units
+ * @property {number} speed-bytes
+ * @property {array<string>} speed-units
+ *
+ * @typedef  {Object} Stats
+ * @property {number} downloadedBytes
+ * @property {number} filesAdded
+ * @property {number} secondsActive
+ * @property {number} sessionCount
+ * @property {number} uploadedBytes
+ *
+ * @typedef {Promise} AxiosPromise
+ */
 const api_parent = {
   request(method, args) {
     return axios.post('/api',
--- src/services/api_torrent.transmission.service.js
+++ src/services/api_torrent.transmission.service.js
@@ -48,6 +48,7 @@ const ARGUMENTS_TORRENT_ALL = {
     "error",
     "id"]
 };
+
 const ARGUMENTS_TORRENT_INFO = {
   "fields":[
     "metadataPercentComplete",
@@ -95,6 +96,28 @@ const ARGUMENTS_TORRENT_PEERS = {
     "id"],
   "ids":[]};
 
+const ARGUMENTS_TORRENT_TRACKERS = {
+  "fields":[
+    "metadataPercentComplete",
+    "desiredAvailable",
+    "downloadedEver",
+    "haveUnchecked",
+    "queuePosition",
+    "trackerStats",
+    "activityDate",
+    "dateCreated",
+    "corruptEver",
+    "uploadRatio",
+    "haveValid",
+    "startDate",
+    "totalSize",
+    "fileStats",
+    "status",
+    "peers",
+    "name",
+    "id"],
+  "ids":[]};
+
 const ARGUMENTS_TORRENT_REMOVE = {
   "delete-local-data":false,
   "ids":[]
@@ -110,57 +133,41 @@ const ARGUMENT_IDS = {
   "ids":[]
 }
 
+/**
+ * @typedef  {Object} Arguments
+ * @property {array<Torrent>} torrents
+ *
+ * @typedef {Object} Response
+ * @property {Arguments} arguments
+ * @property {string} result
+ */
 const api_torrent = {
   /**
-   * @typedef  {Object} Torrent
-   * @property {number} id
-   *
-   * @typedef  {Object} Results
-   * @property {Arguments} arguments
-   * @property {string} result
-   *
-   * @typedef  {Object} Arguments
-   *
    * @param {number} location
    * @param {array<Torrent>} torrents
-   *
-   * @return {Promise<Results>}
+   * @return {AxiosPromise<Response>}
    */
   setLocation(torrents, location) {
     let args = ARGUMENTS_SET_LOCATION;
 
     if (torrents != null) {
-      args.move = true;
-      args.location = location;
       args.ids = torrents.map(torrent => torrent.id);
+      args.location = location;
+      args.move = true;
     }
 
     return api_parent.request(TORRENT_SET_LOCATION, args);
   },
   /**
-   * @typedef  {Object} Torrent
-   * @property {number} id
-   * @property {number} name
-   *
-   * @typedef  {Object} Results
-   * @property {Arguments} arguments
-   * @property {string} result
-   *
-   * @typedef  {Object} Arguments
-   *
    * @param {string} newName
    * @param {Torrent} torrent
-   *
-   * @return {Promise<Results>}
+   * @return {AxiosPromise<Response>}
    */
   setRename(torrent, newName) {
     let args = ARGUMENTS_RENAME;
-
-    if (torrent != null) {
-      args.name = newName;
-      args.path = torrent.name;
-      args.ids.push(torrent.id);
-    }
+    args.ids.push(torrent.id);
+    args.path = torrent.name;
+    args.name = newName;
 
     return api_parent.request(TORRENT_RENAME_PATH, args);
   },
@@ -180,160 +187,98 @@ const api_torrent = {
   },
 
   /**
-   * @typedef  {Object} Torrent
-   * @property {number} id
-   *
-   * @typedef  {Object} Response
-   * @property {string} result
-   *
-   * @param {Torrent} torrent
-   *
-   * @return {Promise<Response>}
+   * @param {array<Torrent>} torrents
+   * @return {AxiosPromise<Response>}
    */
-  verifyTorrent(torrent) {
+  verifyTorrent(torrents) {
     const args = ARGUMENT_IDS;
-
-    if (Array.isArray(torrent)) {
-      args.ids = torrent.map(t => t.id)
-    } else if (torrent !== null && torrent.id !== null) {
-      args.ids = [torrent.id]
-    }
+    args.ids = torrents.map(torrent => torrent.id);
 
     return api_parent.request(TORRENT_VERIFY, args);
   },
 
-  removeTorrent(torrent, trash) {
+  /**
+   * @param {array<Torrent>} torrents
+   * @param {boolean} trash
+   * @return {AxiosPromise<Response>}
+   */
+  removeTorrent(torrents, trash) {
     const args = ARGUMENTS_TORRENT_REMOVE;
+    args.ids = torrents.map(torrent => torrent.id);
     args['delete-local-data'] = trash;
-
-    if (Array.isArray(torrent)) {
-      args.ids = torrent.map(t => t.id)
-    } else if (torrent !== null && torrent.id !== null) {
-      args.ids = [torrent.id]
-    }
 
     return api_parent.request(TORRENT_REMOVE, args);
   },
 
-  startTorrents(torrent) {
+  /**
+   * @param {array<Torrent>} torrents
+   * @return {AxiosPromise<Response>}
+   */
+  startTorrents(torrents) {
     const args = ARGUMENT_IDS;
-
-    if (Array.isArray(torrent)) {
-      args.ids = torrent.map(t => t.id)
-    } else if (torrent !== null && torrent.id !== null) {
-      args.ids = [torrent.id]
-    }
+    args.ids = torrents.map(torrent => torrent.id);
 
     return api_parent.request(TORRENT_START, args);
   },
 
-  startTorrentsNow(torrent) {
+  /**
+   * @param {array<Torrent>} torrents
+   * @return {AxiosPromise<Response>}
+   */
+  startTorrentsNow(torrents) {
     const args = ARGUMENT_IDS;
-
-    if (Array.isArray(torrent)) {
-      args.ids = torrent.map(t => t.id)
-    } else if (torrent !== null && torrent.id !== null) {
-      args.ids = [torrent.id]
-    }
+    args.ids = torrents.map(torrent => torrent.id)
 
     return api_parent.request(TORRENT_START_NOW, args);
   },
 
-  stopTorrents(torrent) {
-    const args = ARGUMENT_IDS;
-
-    if (Array.isArray(torrent)) {
-      args.ids = torrent.map(t => t.id)
-    } else if (torrent !== null && torrent.id !== null) {
-      args.ids = [torrent.id]
-    }
-
-    return api_parent.requestSimple(TORRENT_STOP, torrent);
-  },
   /**
-   *
-   * @typedef  {Object} Torrents
-   * @property {number} id
-   * @property {string} name
-   * @property {number} totalSize
-   * @property {number} dateCreated
-   * @property {number} peersSendingToUs
-   * @property {boolean} isFinished
-   * @property {number} peersConnected
-   * @property {number} percentDone
-   * @property {number} sizeWhenDone
-   * @property {number} rateDownload
-   * @property {number} rateUpload
-   * @property {number} status
-   * @property {number} metadataPercentComplete
-   * @property {number} leftUntilDone
-   * @property {number} uploadedEver
-   * @property {number} uploadRatio
-   * @property {number} seedRatioLimit
-   * @property {number} eta
-   * @property {string} errorString
-   * @property {number} queuePosition
-   * @property {number} activityDate
-   * @property {number} uploadRatio
-   * @property {number} error
-   *
-   * @typedef  {Object} Arguments
-   * @property {array<Torrents>} torrents
-   *
-   * @return {Promise<Arguments>}
+   * @param {array<Torrent>} torrents
+   * @return {AxiosPromise<Response>}
+   */
+  stopTorrents(torrents) {
+    const args = ARGUMENT_IDS;
+    args.ids = torrents.map(torrent => torrent.id)
+
+    return api_parent.request(TORRENT_STOP, args);
+  },
+
+  /**
+   * @return {AxiosPromise<Response>}
    */
   getTorrents() {
     return api_parent.request(TORRENT_GET, ARGUMENTS_TORRENT_ALL);
   },
 
   /**
-   *
-   * @typedef  {Object} Torrent
-   * @property {number} metadataPercentComplete
-   * @property {number} desiredAvailable
-   * @property {number} downloadedEver
-   * @property {boolean} haveUnchecked
-   * @property {number} leftUntilDone
-   * @property {number} activityDate
-   * @property {number} uploadedEver
-   * @property {number} sizeWhenDone
-   * @property {string} downloadDir
-   * @property {string} errorString
-   * @property {number} corruptEver
-   * @property {number} dateCreated
-   * @property {number} failedEver
-   * @property {boolean} isFinished
-   * @property {number} hashString
-   * @property {number} pieceCount
-   * @property {boolean} haveValid
-   * @property {number} startDate
-   * @property {number} pieceSize
-   * @property {number} totalSize
-   * @property {boolean} isPrivate
-   * @property {string} comment
-   * @property {string} creator
-   * @property {number} status
-   * @property {string} name
-   * @property {number} eta
-   * @property {number} id
-   *
-   * @typedef  {Object} Arguments
-   * @property {array<Torrent>} torrents
-   *
    * @param {array<Torrent>} torrents
-   *
-   * @return {Promise<Arguments>}
+   * @return {AxiosPromise<Response>}
    */
   getInfoTorrent(torrents) {
     const args = ARGUMENTS_TORRENT_INFO;
-    args.ids = torrents.map(torrent =>torrent.id)
+    args.ids = torrents.map(torrent => torrent.id)
 
     return api_parent.request(TORRENT_GET, args);
   },
 
+  /**
+   * @param {array<Torrent>} torrents
+   * @return {AxiosPromise<Response>}
+   */
   getPeersTorrent(torrents) {
     const args = ARGUMENTS_TORRENT_PEERS;
-    args.ids = torrents.map(torrent =>torrent.id)
+    args.ids = torrents.map(torrent => torrent.id)
+
+    return api_parent.request(TORRENT_GET, args);
+  },
+
+  /**
+   * @param {array<Torrent>} torrents
+   * @return {AxiosPromise<Response>}
+   */
+  getTrackerTorrent(torrents) {
+    const args = ARGUMENTS_TORRENT_TRACKERS;
+    args.ids = torrents.map(torrent => torrent.id)
 
     return api_parent.request(TORRENT_GET, args);
   }
